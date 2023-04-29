650acf5fb1da66e8a2685e45d232c35f2c8e52b5
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2023-04-29 17:02:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react"
 
-function App() {
+const App = ()=> {
+const [message, setMessage] = useState("")
+const [value,setValue] = useState("")
+const [previousChat,setPreviousChat] = useState([])
+const [currentTitle,setCurrentTitle] = useState("")
+
+
+
+const createNewChat = ()=>{
+  setMessage(null)
+  setValue("")
+  setCurrentTitle("")
+}
+
+const handleClick = (uniqueTitle)=>{
+setCurrentTitle(uniqueTitle)
+setMessage(null)
+setValue("")
+
+}
+
+  const getMessages  = async()=>{
+console.log("clicked")
+    const options = {
+      method:"POST",
+      body:JSON.stringify({
+        message:value
+      }),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+    try {
+      const response = await fetch("http://localhost:8000/completions",options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+   
+      
+    } catch (error) {
+      console.error(error)
+    }
+  }
+console.log(message)
+
+useEffect(()=>{
+console.log(currentTitle,value,message)
+if(!currentTitle && value && message){
+  setCurrentTitle(value)
+}
+if(currentTitle&&value&&message){
+  setPreviousChat(prevchats=>(
+    [...prevchats,{
+title:currentTitle,
+role:"user",
+content:value
+    },{
+      title:currentTitle,
+role:message.role,
+content:message.content,
+    }
+  ]
+  ))
+}
+},[message,currentTitle])
+console.log(previousChat)
+
+const currentChat = previousChat.filter(previousChat=>previousChat.title===currentTitle)
+const uniqueTitles = Array.from(new Set(previousChat.map(previousChat=>previousChat.title)))
+console.log(uniqueTitles)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar">
+        <button onClick={createNewChat}>+ New Chat</button>
+        <ul className="history">
+          {
+            uniqueTitles?.map((uniqueTitle,index)=><li onClick={()=>handleClick(uniqueTitle)} key = {index}>{uniqueTitle}</li>)
+          }
+        </ul>
+        <nav>
+          <p>Made by Ashutosh</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle &&<h1>AshuGPT</h1>}
+        <ul className="feed">
+{currentChat?.map((chatmessage,index)=><li key = {index}>
+  <p className="role">{chatmessage.role}</p>
+  <p>{chatmessage.content}</p>
+</li>)}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input  value={value} onChange={(e)=>setValue(e.target.value)}/>
+            <div onClick={getMessages} id="submit">➢</div>
+          </div>
+          <p className="info">
+          We’ve trained a model called ChatGPT which interacts in a conversational way. 
+          The dialogue format makes it possible for ChatGPT to answer 
+          followup questions, admit its mistakes, challenge incorrect premises, 
+          and reject inappropriate requests.
+          </p>
+        </div>
+      </section>
+
     </div>
   );
 }
